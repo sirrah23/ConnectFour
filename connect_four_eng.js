0ca6edacf93444ae11a54d502d40ce3cac1d4d0a
sirrah23 <hseth93@gmail.com>
2018-01-25 00:41:35 -0500

    Added scan to see if a player has won the game yet

--- connect_four_eng.js
+++ connect_four_eng.js
@@ -92,15 +92,212 @@ ConnectFour.prototype.make_move = function(col){
 * Scan the board to see if anyone has won yet.
 */
 ConnectFour.prototype.scan_winner = function(){
-    //TODO
-}
+    let potential_winner;
+
+    console.log("Column scan");
+    potential_winner = this.scan_winner_col();
+    if(potential_winner !== -1){
+        return potential_winner;
+    }
+
+    console.log("Row scan");
+    potential_winner = this.scan_winner_row();
+    if(potential_winner !== -1){
+        return potential_winner;
+    }
+
+    console.log("Diag-left scan");
+    potential_winner = this.scan_winner_diag_left();
+    if(potential_winner !== -1){
+        return potential_winner;
+    }
+
+    console.log("Diag-right scan");
+    potential_winner = this.scan_winner_diag_right();
+    if(potential_winner !== -1){
+        return potential_winner;
+    }
+    return -1;
+};
+
+ConnectFour.prototype.scan_winner_col = function(){
+    let potential_winner, stream_size;
+    for(let i = 0; i < this.cols; i++){
+        potential_winner = null;
+        stream_size = null;
+        for(let j = 0; j < this.rows; j++){
+            if (this.board[i][j] === 0){
+                potential_winner = null;
+                stream_size = null;
+            } else if(this.board[i][j] === potential_winner){
+                stream_size++;
+            } else {
+                potential_winner = this.board[i][j];
+                stream_size = 1;
+            }
+
+            if(stream_size === 4){
+                return potential_winner;
+            }
+        }
+    }
+    return -1;
+};
 
 
-/*
+ConnectFour.prototype.scan_winner_row = function(){
+    let potential_winner, stream_size;
+    for(let i = 0; i < this.rows; i++){
+        potential_winner = null;
+        stream_size = null;
+        for(let j = 0; j < this.cols; j++){
+            if (this.board[j][i] === 0){
+                potential_winner = null;
+                stream_size = null;
+            } else if(this.board[j][i] === potential_winner){
+                stream_size++;
+            } else {
+                potential_winner = this.board[j][i];
+                stream_size = 1;
+            }
+
+            if(stream_size === 4){
+                return potential_winner;
+            }
+        }
+    }
+    return -1;
+};
+
+
+ConnectFour.prototype.scan_winner_diag_left = function(){
+    let potential_winner, stream_size, step;
+    let row_pos, col_pos;
+
+    //Scan by walking down first column
+    row_pos = 0, col_pos = 0;
+    for(; row_pos < this.rows; row_pos++){
+        potential_winner = null;
+        stream_size = null;
+        step = 0;
+        while(true){
+            if ((row_pos + step > this.rows) || (col_pos + step > this.cols)){
+                break;
+            }
+            if (this.board[row_pos + step][col_pos + step] === 0){
+                potential_winner = null;
+                stream_size = null;
+            } else if (this.board[row_pos + step][col_pos + step] === potential_winner){
+                stream_size++;
+            } else {
+                potential_winner = this.board[row_pos + step][col_pos + step];
+                stream_size = 1;
+            }
+
+            if(stream_size === 4){
+                return potential_winner;
+            }
+
+            step++;
+        }
+    }
+
+    //Scan by walking across first row
+    row_pos = 0, col_pos = 0;
+    for(; col_pos < this.cols; col_pos++){
+        potential_winner = null;
+        stream_size = null;
+        step = 0;
+        while(true){
+            if ((row_pos + step > this.rows) || (col_pos + step > this.cols)){
+                break;
+            }
+            if (this.board[row_pos + step][col_pos + step] === 0){
+                potential_winner = null;
+                stream_size = null;
+            } else if (this.board[row_pos + step][col_pos + step] === potential_winner){
+                stream_size++;
+            } else {
+                potential_winner = this.board[row_pos + step][col_pos + step];
+                stream_size = 1;
+            }
+
+            if(stream_size === 4){
+                return potential_winner;
+            }
+
+            step++;
+        }
+    }
+
+    return -1;
+};
+
+ConnectFour.prototype.scan_winner_diag_right = function(){
+    let potential_winner, stream_size, step;
+    let row_pos, col_pos;
+
+    //Scan by walking down last column
+    row_pos = 0, col_pos = this.cols-1;
+    for(; row_pos < this.rows; row_pos++){
+        potential_winner = null;
+        stream_size = null;
+        step = 0;
+        while(true){
+            if ((row_pos + step > this.rows) || (col_pos - step < 0)){
+                break;
+            }
+            if (this.board[row_pos + step][col_pos - step] === 0){
+                potential_winner = null;
+                stream_size = null;
+            } else if (this.board[row_pos + step][col_pos - step] === potential_winner){
+                stream_size++;
+            } else {
+                potential_winner = this.board[row_pos + step][col_pos - step];
+                stream_size = 1;
+            }
+
+            if(stream_size === 4){
+                return potential_winner;
+            }
+
+            step++;
+        }
+    }
+
+    //Scan by walking across first row backwards
+    row_pos = 0, col_pos = this.cols-1;
+    for(; col_pos >= 0; col_pos--){
+        potential_winner = null;
+        stream_size = null;
+        step = 0;
+        while(true){
+            if ((row_pos + step > this.rows) || (col_pos - step < 0)){
+                break;
+            }
+            if (this.board[row_pos + step][col_pos - step] === 0){
+                potential_winner = null;
+                stream_size = null;
+            } else if (this.board[row_pos + step][col_pos - step] === potential_winner){
+                stream_size++;
+            } else {
+                potential_winner = this.board[row_pos + step][col_pos - step];
+                stream_size = 1;
+            }
+
+            if(stream_size === 4){
+                return potential_winner;
+            }
+
+            step++;
+        }
+    }
+
+    return -1;
+};
+
+
+
 const c4 = new ConnectFour();
-c4.make_move(1);
-c4.make_move(2);
-c4.make_move(1);
-c4.make_move(2);
+console.log(c4.scan_winner());
 console.log(c4.toString());
-*/
